001e3832ece933db094c88a14140148bfc21dc2e
jghodd <jghodd@gmail.com>
2018-03-16 14:13:37 -0400

    Create prefs.js

---
+++ src/defaults/preferences/prefs.js
@@ -0,0 +1,3 @@
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.lattedock-unread.description", "chrome://lattedockunread/locale/lattedock-unread.properties");
+pref("extensions.lattedock-unread.traverse-deep", false);
